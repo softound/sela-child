1e0e43d08b1ac2adb4a8f06d0964136579f5e5ee
Softound Solutions <softound@gmail.com>
2016-07-27 16:44:28 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Load the parent theme style.css
+ */
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+
+
+?>
